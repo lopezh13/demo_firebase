2e5a715d62ad915b820f55cd7469a3c7a3efb562
lopezh13 <mario.lopez22@itca.edu.sv>
2023-09-19 15:43:25 -0600

    nuevos cambios

---
+++ lib/pages/home.dart
@@ -0,0 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void getData() async {
+     CollectionReference collectionReference = 
+     FirebaseFirestore.instance.collection("estudiante"); 
+     QuerySnapshot mensajes = await collectionReference.get(); 
+     if(mensajes.docs.length != 0){ 
+      for (var doc in mensajes.docs){
+        print(doc.data()); 
+        //chatsx.add(doc.data()); 
+      }
+    } 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+           
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+@override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+
+}
